10b45a08bd87175cc932464e6502a381f62279b5
saikanth reddy <medasaikanth@gmail.com>
2018-08-15 03:02:26 +0530

    dishDetail component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import {View} from 'react-native'
 
 import Menu from './components/Menu'
+import DishDetail from './components/DishDetail'
+
 import DISHES from './shared/Dishes'
 
 export default class Main extends Component {
@@ -8,13 +11,26 @@ export default class Main extends Component {
         super(props)
 
         this.state = { 
-            dishes: DISHES
+            dishes: DISHES,
+            selectedDish: null
+        };
+
+        onSelectDish(dishId) {
+            this.setState({selectedDish: dishId})
         }
     }
 
     render() {
         return (
-            <Menu dishes={this.state.dishes}/>
+            <View style={{flex: 1}}>
+                <Menu 
+                    dishes={this.state.dishes}
+                    onPress={(dishId) => this.onSelectDish(dishId)}
+                />
+                <DishDetail 
+                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}
+                />
+            </View>
         )
     }
 } 
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -13,6 +13,7 @@ function Menu(props) {
                 title={item.name}
                 subtitle={item.description}
                 hideChevron={true}
+                onPress={props.onPress(item.id)}
             />
         )
     }
